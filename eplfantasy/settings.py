0f61db3098c088b506bde381d436d3095df2cea2
ronnkarani <ronnkarani25@gmail.com>
2025-07-26 19:29:51 +0300

    Initial commit for Render deployment

--- eplfantasy/settings.py
+++ eplfantasy/settings.py
@@ -1,3 +1,5 @@
+from decouple import config
+
 import os
 from pathlib import Path
 
@@ -9,12 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sx0qdhbqliv5)up5ovd0ft+3pmfrhtpxevlp3g$=9_3u$ieg@o'
 
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*', 'localhost', ]
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
